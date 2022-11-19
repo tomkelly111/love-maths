7885e419a94a00d69023b00f322cbe5ba26ffbeb
tomkelly111 <thomaspkelly111@gmail.com>
2022-11-19 11:54:41 +0000

    add event listners for document loading and button clicks

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,48 @@
-console.log("connected!")
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clickedSubmit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        }
+        )
+    }
+})
+
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
